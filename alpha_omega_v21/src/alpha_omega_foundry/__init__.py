5d146cee1b713099d1ed66dfa1857360b8d61f60
mosianekk-lang <mosianekk@gmail.com>
2026-08-03 10:06:57 +0200

    Add Alpha Omega Foundry v2.1 package exports

---
+++ alpha_omega_v21/src/alpha_omega_foundry/__init__.py
@@ -0,0 +1,10 @@
+from .foundry import SolutionFoundry
+from .operations import OperationsFabric
+from .providers import LocalProviderAdapter, ReleaseArtifactAdapter
+
+__all__ = [
+    "SolutionFoundry",
+    "OperationsFabric",
+    "LocalProviderAdapter",
+    "ReleaseArtifactAdapter",
+]
