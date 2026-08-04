a2035577b510f8daaab9a9bebaa60fa8785dedb3
mosianekk-lang <mosianekk@gmail.com>
2026-08-04 15:09:20 +0200

    Add Live Bible v2 package initializer

---
+++ live_bible_capture/releases/v2.0.0/source/live_bible_fabric/__init__.py
@@ -0,0 +1,6 @@
+"""Federation Omega Live Bible Capture Fabric v2.0."""
+
+from .fabric import CaptureEvent, EventStore, LiveBibleEngine
+
+__all__ = ["CaptureEvent", "EventStore", "LiveBibleEngine"]
+__version__ = "2.0.0"
