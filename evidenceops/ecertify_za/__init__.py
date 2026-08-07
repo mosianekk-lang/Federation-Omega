c7759c06191d0e88996b47ba5b504d75e3548f81
mosianekk-lang <mosianekk@gmail.com>
2026-08-07 12:32:19 +0200

    feat(ecertify): expose v0.4 production-boundary interfaces

--- evidenceops/ecertify_za/__init__.py
+++ evidenceops/ecertify_za/__init__.py
@@ -2,8 +2,11 @@
 from .identity_receipt import IdentityReceiptGate
 from .legal import CertificationRouteEngine
 from .ledger import HashChainLedger
+from .provider_adapter import IdentityProviderAdapter,ProviderCapabilities
 from .receipt_auth import HMACReceiptAuthenticator,ReceiptEnvelope,ReplayStore
+from .replay import PostgresReplayGuard,ReplayGuard,SQLiteReplayGuard
 from .service import ECertifyService
+from .verification_registry import PublicVerification,SQLiteVerificationRegistry
 
-__all__=["IdentityReceiptGate","CertificationRouteEngine","HashChainLedger","HMACReceiptAuthenticator","ReceiptEnvelope","ReplayStore","ECertifyService"]
-__version__="0.3.0"
+__all__=["IdentityReceiptGate","CertificationRouteEngine","HashChainLedger","IdentityProviderAdapter","ProviderCapabilities","HMACReceiptAuthenticator","ReceiptEnvelope","ReplayStore","ReplayGuard","SQLiteReplayGuard","PostgresReplayGuard","PublicVerification","SQLiteVerificationRegistry","ECertifyService"]
+__version__="0.4.0"
