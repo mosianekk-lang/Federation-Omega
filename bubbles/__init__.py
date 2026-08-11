816316a65aa1047cd084072cf7bdcfcdff6d1f5b
mosianekk-lang <mosianekk@gmail.com>
2026-08-12 01:34:01 +0200

    feat: export Bubbles architect twin package

---
+++ bubbles/__init__.py
@@ -0,0 +1,13 @@
+from .architect_twin import (
+    BubblesArchitectTwin,
+    MaturityStage,
+    Project,
+    ProjectAssessment,
+)
+
+__all__ = [
+    "BubblesArchitectTwin",
+    "MaturityStage",
+    "Project",
+    "ProjectAssessment",
+]
