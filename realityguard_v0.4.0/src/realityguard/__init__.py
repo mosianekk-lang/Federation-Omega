8239bdf1d8d7ea22d6f161ca4a2c2e44049bf005
mosianekk-lang <mosianekk@gmail.com>
2026-08-15 07:24:10 +0200

    RealityGuard 0.4.0: update src/realityguard/__init__.py for universal Federation adapter

--- realityguard_v0.4.0/src/realityguard/__init__.py
+++ realityguard_v0.4.0/src/realityguard/__init__.py
@@ -6,11 +6,13 @@ from .capability import CapabilityRegistry, CapabilityState
 from .solutions import ReuseAction, SolutionRouter
 from .prebuild import PrebuildDecisionCode, PrebuildGate, manifest_snapshot_hash
 from .upgrade import CycleKind, GovernedUpgradeEngine, UpgradeDecisionCode
+from .federation_adapter import FederationUpgradeAdapter
 
 __all__ = [
     "RealityGuard", "EvidenceGrade", "LifecycleState", "Verdict",
     "CapabilityRegistry", "CapabilityState", "ReuseAction", "SolutionRouter",
     "PrebuildDecisionCode", "PrebuildGate", "manifest_snapshot_hash",
     "CycleKind", "GovernedUpgradeEngine", "UpgradeDecisionCode",
+    "FederationUpgradeAdapter",
 ]
 __version__ = "0.4.0"
