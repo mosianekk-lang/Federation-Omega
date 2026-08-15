cc96e1dc15baa66b6de6cf54f6469674b0f6c1bd
mosianekk-lang <mosianekk@gmail.com>
2026-08-15 02:31:05 +0200

    ChatGov Ω3.1: bind completion interlock tests into Airlock

---
+++ tests/test_phoenix_provider_cutover_v3_chatgov_completion_interlock.py
@@ -0,0 +1,5 @@
+"""Airlock regression binding for ChatGov × ChatBridge completion witnessing."""
+
+from bubbles.chat_governor_omega3.test_completion import ChatGovCompletionInterlockTests
+
+__all__ = ["ChatGovCompletionInterlockTests"]
