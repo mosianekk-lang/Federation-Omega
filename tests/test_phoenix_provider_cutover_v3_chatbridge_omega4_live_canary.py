be43520081609a3999a5377c40b1397b461d611e
mosianekk-lang <mosianekk@gmail.com>
2026-08-15 02:27:37 +0200

    ChatBridge Ω4.3: bind live canary safety tests into Airlock

---
+++ tests/test_phoenix_provider_cutover_v3_chatbridge_omega4_live_canary.py
@@ -0,0 +1,5 @@
+"""Airlock regression binding for ChatBridge Ω4 provider-live canary harness."""
+
+from bubbles.chatbridge_omega4.test_live_canary import ChatBridgeOmega4LiveCanaryTests
+
+__all__ = ["ChatBridgeOmega4LiveCanaryTests"]
