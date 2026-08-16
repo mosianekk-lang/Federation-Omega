ee51a6a22e4aca8e586283791433faf4ecee4673
mosianekk-lang <mosianekk@gmail.com>
2026-08-16 22:38:09 +0200

    ChatBridge Ω4.7: bind scope promotion regressions into Airlock

--- tests/test_phoenix_provider_cutover_v3_chatbridge_omega4_empirical_playbook.py
+++ tests/test_phoenix_provider_cutover_v3_chatbridge_omega4_empirical_playbook.py
@@ -1,4 +1,7 @@
 from bubbles.chatbridge_omega4.test_empirical_playbook import EmpiricalPlaybookTests
+from bubbles.chatbridge_omega4.test_empirical_playbook_scope import (
+    EmpiricalPlaybookScopeTests,
+)
 
 
-__all__ = ["EmpiricalPlaybookTests"]
+__all__ = ["EmpiricalPlaybookTests", "EmpiricalPlaybookScopeTests"]
