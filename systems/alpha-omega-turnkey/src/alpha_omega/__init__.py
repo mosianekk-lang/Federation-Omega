fc64b7b56166ded8cb42d9317b14064e37a11ebe
mosianekk-lang <mosianekk@gmail.com>
2026-08-23 11:36:18 +0200

    Export progressive Alpha-Omega runtime

--- systems/alpha-omega-turnkey/src/alpha_omega/__init__.py
+++ systems/alpha-omega-turnkey/src/alpha_omega/__init__.py
@@ -1,3 +1,33 @@
 from .engine import AlphaOmegaEngine
+from .progressive import (
+    AccelerationProfile,
+    EffectClass,
+    FormationInnovationEngine,
+    HashLinkedLearningLedger,
+    MultiStreamScheduler,
+    PathKind,
+    ProgressiveAlphaOmega,
+    ProgressivePlan,
+    RouteCandidate,
+    StreamUnit,
+    UnitState,
+    WaveDecision,
+    WaveExecutionReceipt,
+)
 
-__all__ = ["AlphaOmegaEngine"]
+__all__ = [
+    "AccelerationProfile",
+    "AlphaOmegaEngine",
+    "EffectClass",
+    "FormationInnovationEngine",
+    "HashLinkedLearningLedger",
+    "MultiStreamScheduler",
+    "PathKind",
+    "ProgressiveAlphaOmega",
+    "ProgressivePlan",
+    "RouteCandidate",
+    "StreamUnit",
+    "UnitState",
+    "WaveDecision",
+    "WaveExecutionReceipt",
+]
