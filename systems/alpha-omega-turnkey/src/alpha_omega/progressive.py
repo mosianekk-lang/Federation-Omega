aeea96098ba89217468a8b7f8224f48566e8dc7e
mosianekk-lang <mosianekk@gmail.com>
2026-08-23 11:46:25 +0200

    Add progressive runtime public interface

---
+++ systems/alpha-omega-turnkey/src/alpha_omega/progressive.py
@@ -0,0 +1,31 @@
+from .progressive_formation import FormationInnovationEngine
+from .progressive_ledger import HashLinkedLearningLedger
+from .progressive_models import (
+    AccelerationProfile,
+    EffectClass,
+    PathKind,
+    ProgressivePlan,
+    RouteCandidate,
+    StreamUnit,
+    UnitState,
+    WaveDecision,
+    WaveExecutionReceipt,
+)
+from .progressive_runtime import ProgressiveAlphaOmega
+from .progressive_scheduler import MultiStreamScheduler
+
+__all__ = [
+    "AccelerationProfile",
+    "EffectClass",
+    "FormationInnovationEngine",
+    "HashLinkedLearningLedger",
+    "MultiStreamScheduler",
+    "PathKind",
+    "ProgressiveAlphaOmega",
+    "ProgressivePlan",
+    "RouteCandidate",
+    "StreamUnit",
+    "UnitState",
+    "WaveDecision",
+    "WaveExecutionReceipt",
+]
