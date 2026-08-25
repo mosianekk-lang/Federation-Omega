fa2db0516ce89b5529e699c7362ef044418d9b79
mosianekk-lang <mosianekk@gmail.com>
2026-08-25 12:22:54 +0200

    Formation-Omega: add package exports

---
+++ formation_omega/__init__.py
@@ -0,0 +1,17 @@
+"""Public-safe Formation-Omega convergence primitives."""
+
+from .powerhouse import (
+    ClaimDecision,
+    FormationOmega,
+    ProofState,
+    ReleaseGate,
+    SurfaceReadback,
+)
+
+__all__ = [
+    "ClaimDecision",
+    "FormationOmega",
+    "ProofState",
+    "ReleaseGate",
+    "SurfaceReadback",
+]
