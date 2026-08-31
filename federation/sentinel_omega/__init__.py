46758d8c019164c769afba12a502c17408b68c42
mosianekk-lang <mosianekk@gmail.com>
2026-08-31 23:06:20 +0200

    feat(sentinel): expose restacked observation ingress adapters

--- federation/sentinel_omega/__init__.py
+++ federation/sentinel_omega/__init__.py
@@ -9,13 +9,25 @@ from .observability_causal_fabric import (
     SignalKind,
     SLOWindowSample,
 )
+from .observation_ingress import (
+    GitHubWorkflowRunAdapter,
+    HeartbeatObservationAdapter,
+    ObservationIngressBatch,
+    ProjectionDriftObservationAdapter,
+    QueueObservationAdapter,
+)
 from .topology_correlation import SentinelObservabilityCausalFabric
 
 __all__ = [
     "AdaptiveBaselineDetector",
+    "GitHubWorkflowRunAdapter",
+    "HeartbeatObservationAdapter",
     "IncidentCorrelator",
     "MultiWindowSLOGuard",
     "NormalizedObservation",
+    "ObservationIngressBatch",
+    "ProjectionDriftObservationAdapter",
+    "QueueObservationAdapter",
     "SemanticObservationNormalizer",
     "SentinelObservabilityCausalFabric",
     "SignalKind",
