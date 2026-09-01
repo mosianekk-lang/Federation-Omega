aebccc23a6e5c50d63a3632809b4f2ef8c979fee
mosianekk-lang <mosianekk@gmail.com>
2026-09-01 10:35:31 +0200

    feat(cfbe): register current Level 7+ source signal projection

---
+++ benchmarking/cfbe_omega/kim_dataverse_level7_plus_current_source_signals_v1.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+
+def current_source_signals() -> dict[str, bool]:
+    return {
+        "objective_ecology": True,
+        "resource_economy": True,
+        "owner_interruption_firewall": True,
+        "autonomy_debt": True,
+        "dynamic_topology": True,
+        "digital_twin": True,
+        "measured_gap_evolution": True,
+        "historical_replay": True,
+        "adversarial_qualification": True,
+        "architectural_entropy_controller": True,
+        "causal_learning": True,
+        "no_self_authority_promotion": True,
+        "unified_autonomic_loops": True,
+        "dynamic_reorganization": True,
+        "multi_timescale_objective_optimization": True,
+        "constitutional_amendment_court": True,
+        "capability_market": True,
+        "cross_provider_counterfactuals": True,
+        "information_value_budgeting": True,
+        "negative_knowledge_diffusion": True,
+        "persistent_no_chat_continuity": False,
+        "irreducible_owner_interruptions_only": False,
+        "verified_value_retention": False,
+        "lane_local_failure_isolation": False,
+    }
