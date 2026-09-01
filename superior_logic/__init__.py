2e677aedc3dc49f0410eb4c60f7adc586827c5eb
mosianekk-lang <mosianekk@gmail.com>
2026-09-01 12:52:33 +0200

    feat(slos): expose opportunity discovery engine

--- superior_logic/__init__.py
+++ superior_logic/__init__.py
@@ -6,6 +6,7 @@ from .digital_twin import CapabilityEdge, FederationDigitalTwin, RouteCandidate
 from .evidence_distillation import EvidenceDistiller, EvidenceReceipt
 from .hyperperformance import CounterfactualRoute, HyperperformanceController, MissionPlan
 from .mission_ir import HyperSchedule, LaneClass, MissionCompiler, MissionIR, MissionNode, ParallelWave
+from .opportunity_discovery import Opportunity, OpportunityDiscoveryEngine
 from .shadow_evolution import PromotionDecision, ShadowEvolutionEngine, TrialScore
 
 __all__ = [
@@ -21,6 +22,8 @@ __all__ = [
     "MissionIR",
     "MissionNode",
     "MissionPlan",
+    "Opportunity",
+    "OpportunityDiscoveryEngine",
     "ParallelWave",
     "PromotionDecision",
     "RouteCandidate",
