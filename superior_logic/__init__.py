a55f6ff3120dd81c376ad1f5d28026d16eff84bf
mosianekk-lang <mosianekk@gmail.com>
2026-09-01 12:48:41 +0200

    feat(slos): expose hyperperformance kernel version

--- superior_logic/__init__.py
+++ superior_logic/__init__.py
@@ -1,3 +1,25 @@
 """Federation Omega Superior Logic runtime."""
 
-__version__ = "3.2.0"
+__version__ = "3.3.0"
+
+from .digital_twin import CapabilityEdge, FederationDigitalTwin, RouteCandidate
+from .evidence_distillation import EvidenceDistiller, EvidenceReceipt
+from .mission_ir import HyperSchedule, LaneClass, MissionCompiler, MissionIR, MissionNode, ParallelWave
+from .shadow_evolution import PromotionDecision, ShadowEvolutionEngine, TrialScore
+
+__all__ = [
+    "CapabilityEdge",
+    "EvidenceDistiller",
+    "EvidenceReceipt",
+    "FederationDigitalTwin",
+    "HyperSchedule",
+    "LaneClass",
+    "MissionCompiler",
+    "MissionIR",
+    "MissionNode",
+    "ParallelWave",
+    "PromotionDecision",
+    "RouteCandidate",
+    "ShadowEvolutionEngine",
+    "TrialScore",
+]
