22d200921ca2608a69a1bbbccef5de46c5eeb7ea
mosianekk-lang <mosianekk@gmail.com>
2026-09-11 23:22:28 +0200

    FUSE Astra+: add protocol awareness package

---
+++ fuse_astra_protocol_awareness/__init__.py
@@ -0,0 +1,23 @@
+from .awareness import (
+    CommunicationIntent,
+    NegotiatedProtocol,
+    ProtocolAwarenessEngine,
+    ProtocolObservation,
+    ProtocolPolicy,
+    ProtocolProfile,
+    ProtocolRegistry,
+    ProtocolSelectionError,
+    default_protocol_registry,
+)
+
+__all__ = [
+    "CommunicationIntent",
+    "NegotiatedProtocol",
+    "ProtocolAwarenessEngine",
+    "ProtocolObservation",
+    "ProtocolPolicy",
+    "ProtocolProfile",
+    "ProtocolRegistry",
+    "ProtocolSelectionError",
+    "default_protocol_registry",
+]
