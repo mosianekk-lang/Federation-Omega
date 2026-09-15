618106272651b7ef13a8225e2ccd3d2723fd0c33
mosianekk-lang <mosianekk@gmail.com>
2026-09-15 04:26:21 +0200

    Genesis MVS: emit frozen court wrappers

--- genesis/include/fuse/court.h
+++ genesis/include/fuse/court.h
@@ -10,4 +10,33 @@ DECL_IO(court_pth_001);DECL_IO(court_pth_002);DECL_IO(court_pth_003);DECL_IO(cou
 DECL_IO(court_fil_001);DECL_IO(court_fil_002);DECL_IO(court_fil_003);DECL_IO(court_fil_004);DECL_IO(court_fil_005);
 DECL_IO(court_wrt_001);DECL_IO(court_wrt_002);DECL_IO(court_wrt_003);DECL_IO(court_wrt_004);DECL_IO(court_wrt_005);DECL_IO(court_wrt_006);DECL_IO(court_wrt_007);
 #undef DECL_IO
+
+#define court_fmt_001 (+[](CourtContext*c)->CourtResult{return fmt_case("FMT-001",c);})
+#define court_fmt_002 (+[](CourtContext*c)->CourtResult{return fmt_case("FMT-002",c);})
+#define court_fmt_003 (+[](CourtContext*c)->CourtResult{return fmt_case("FMT-003",c);})
+#define court_fmt_004 (+[](CourtContext*c)->CourtResult{return fmt_case("FMT-004",c);})
+#define court_fmt_005 (+[](CourtContext*c)->CourtResult{return fmt_case("FMT-005",c);})
+#define court_fmt_006 (+[](CourtContext*c)->CourtResult{return fmt_case("FMT-006",c);})
+#define court_fmt_007 (+[](CourtContext*c)->CourtResult{return fmt_case("FMT-007",c);})
+#define court_fmt_008 (+[](CourtContext*c)->CourtResult{return fmt_case("FMT-008",c);})
+#define court_jrn_001 (+[](CourtContext*c)->CourtResult{return journal_case("JRN-001",c);})
+#define court_jrn_002 (+[](CourtContext*c)->CourtResult{return journal_case("JRN-002",c);})
+#define court_jrn_003 (+[](CourtContext*c)->CourtResult{return journal_case("JRN-003",c);})
+#define court_jrn_004 (+[](CourtContext*c)->CourtResult{return journal_case("JRN-004",c);})
+#define court_jrn_005 (+[](CourtContext*c)->CourtResult{return journal_case("JRN-005",c);})
+#define court_jrn_006 (+[](CourtContext*c)->CourtResult{return journal_case("JRN-006",c);})
+#define court_jrn_007 (+[](CourtContext*c)->CourtResult{return journal_case("JRN-007",c);})
+#define court_jrn_008 (+[](CourtContext*c)->CourtResult{return journal_case("JRN-008",c);})
+#define court_jrn_009 (+[](CourtContext*c)->CourtResult{return journal_case("JRN-009",c);})
+#define court_jrn_010 (+[](CourtContext*c)->CourtResult{return journal_case("JRN-010",c);})
+#define court_obj_001 (+[](CourtContext*c)->CourtResult{return object_case("OBJ-001",c);})
+#define court_obj_002 (+[](CourtContext*c)->CourtResult{return object_case("OBJ-002",c);})
+#define court_obj_003 (+[](CourtContext*c)->CourtResult{return object_case("OBJ-003",c);})
+#define court_obj_004 (+[](CourtContext*c)->CourtResult{return object_case("OBJ-004",c);})
+#define court_obj_005 (+[](CourtContext*c)->CourtResult{return object_case("OBJ-005",c);})
+#define court_obj_006 (+[](CourtContext*c)->CourtResult{return object_case("OBJ-006",c);})
+#define court_sec_001 (+[](CourtContext*c)->CourtResult{return sec_case("SEC-001",c);})
+#define court_sec_002 (+[](CourtContext*c)->CourtResult{return sec_case("SEC-002",c);})
+#define court_sec_003 (+[](CourtContext*c)->CourtResult{return sec_case("SEC-003",c);})
+#define court_sec_004 (+[](CourtContext*c)->CourtResult{return sec_case("SEC-004",c);})
 }
